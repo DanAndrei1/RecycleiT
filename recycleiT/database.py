947900143d45fffdb32ade9a668708ef0b5f6e90
Cozma Alexandru <alexcozma1112@gmail.com>
2023-11-19 00:48:00 +0200

    bau

--- recycleiT/database.py
+++ recycleiT/database.py
@@ -7,7 +7,8 @@ def load_user(user_id):
     cursor.execute('select * from public.users where id = %s', (user_id,))
     row = cursor.fetchall()
     try:
-        user = User(id=row[0][0], username=row[0][1], email=row[0][2], password=row[0][3], budget=row[0][4])
+        user = User(id=row[0][0], lastName=row[0][1], firstName=row[0][2], username=row[0][3], email=row[0][4],
+                    totalPoints=row[0][5], password=row[0][6])
         return user
     except Exception:
         return None
