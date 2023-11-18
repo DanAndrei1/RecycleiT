4ac77512e72d10687d6bd8b63b5641cd814c58be
DanAndrei1 <dan.andrei.cojocaru@gmail.com>
2023-11-18 17:41:54 +0200

    routes

--- recycleiT/routes.py
+++ recycleiT/routes.py
@@ -1,4 +1,14 @@
 import os
+import uuid
+
+from flask import render_template, request, jsonify, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required, current_user
+
+from database import *
+from server import app
+from models import User
+from forms import RegisterForm, LoginForm
+from __init__ import bcrypt
 
 from __init__ import app
 
@@ -6,7 +16,7 @@ from __init__ import app
 @app.route("/")
 @app.route("/home")
 def index():
-    return "Hello"
+    return render_template('home.html')
 
 
 @app.route("/leaderboard")
@@ -14,24 +24,35 @@ def leaderboard():
     return "Leaderboard"
 
 
-@app.route("/profile")
-def profile():
-    return "Profile"
+@app.route('/profile')
+@login_required
+def about():
+    #rows = get_all()
+    items = [User(id=row[0], firstName=row[1], lastName=row[2], username=row[3], email=row[4], password=row[5], totalPoints=row[6]) for row in rows]
+    return render_template('profile.html', items=items)
 
-
-@app.route("/guide")
+@app.route('/guide')
 def guide():
-    return "Guide"
+    return render_template('guide.html')
 
 
-@app.route("/maps")
+@app.route('/map')
 def maps():
-    return "Map"
+    return render_template('map.html')
 
 
 @app.route('/login')
 def login():
-    return 'Login'
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = get_user_by_username(usernameToCheck=form.username.data)
+        if attempted_user is not None and bcrypt.check_password_hash(attempted_user.get_password(), form.password.data):
+            login_user(attempted_user)
+            flash(f'Succes! You are logged in as:{attempted_user.username}', category='success')
+            return redirect(url_for('about'))
+        else:
+            flash('Username and password are not match! Try again', category='danger')
+    return render_template('login.html', form=form)
 
 
 @app.route('/register')
